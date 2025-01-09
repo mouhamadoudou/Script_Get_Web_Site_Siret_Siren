5287c7252fe1a0f930b3653a3358863edee113dd
ProjectKendara <projectkendara@192.168.1.35>
2025-01-09 09:42:26 +0000

    updt

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 
 CORS(app)
 
-socketio = SocketIO(app, cors_allowed_origins="*")  
+socketio = SocketIO(app, cors_allowed_origins="*")
+
 
 
 def write_to_file(data):
@@ -17,36 +18,45 @@ def write_to_file(data):
         for item in data:
             file.write(item + "\n")
 
-                
 def lunchApi(typePro, data):
     try:
-        if(typePro == "oneUrl") :
+        if typePro == "oneUrl":
             write_to_file([data["url"]])
             result = analyseSite()
         else:
             result = analyseSite(data)
+
         print("Analyse terminée")
+
         
         socketio.emit('analyse_done', {'message': 'Analyse terminée', 'data': result})
+
     except Exception as e:
         print(f"Error during analysis: {e}")
         socketio.emit('analyse_done', {'message': 'Erreur lors de l\'analyse', 'data': str(e)})
 
+
 @app.route('/api/analyse', methods=['POST'])
 def analyse():
-    thread = threading.Thread(target=lunchApi(lunchApi("multipleUrl", "data")))
+    data = request.get_json()  
+    
+    thread = threading.Thread(target=lunchApi, args=("multipleUrl", data))
     thread.start()
 
     return jsonify({"message": "Analyse en cours, vous serez notifié quand elle sera terminée"}), 202
 
-@app.route('/api/analyse/one-url', methods=['POST'])
+
+@app.route('/api/analyse/oneUrl', methods=['POST'])
 def analyseOneLink():
-    data = request.get_json()
+    print("elaaaaaaaaaa")
+    data = request.get_json()  
     print(data)
-    thread = threading.Thread(target=lunchApi("oneUrl", data))
+    
+    thread = threading.Thread(target=lunchApi, args=("oneUrl", data))
     thread.start()
 
     return jsonify({"message": "Analyse en cours, vous serez notifié quand elle sera terminée"}), 202
+
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
