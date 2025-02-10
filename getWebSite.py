0291080556bab18c9db8942affb29cc27caf45e6
ProjectKendara <mouhamadou.niang@epitech.eu>
2025-02-10 08:36:44 +0000

    updt

--- getWebSite.py
+++ getWebSite.py
@@ -19,7 +19,7 @@ def get_google_results(query, num_pages=3):
     all_urls = set()
 
     for page in range(num_pages):
-        url = f"https://www.google.com/search?q={query}&start={page * 10}"
+        url = f"https://www.google.com/search?q={query}&start={page * 2}"
         response = requests.get(url, headers=headers)
         if response.status_code != 200:
             print(f"Erreur lors de la récupération de la page {page + 1}")
@@ -38,9 +38,5 @@ def get_google_results(query, num_pages=3):
                     all_urls.add(real_url)
         time.sleep(random.uniform(1, 3))
 
+    print(all_urls)
     return list(all_urls)
-
-query = "vetement france"
-sites = get_google_results(query)
-for site in sites:
-    print(site)
